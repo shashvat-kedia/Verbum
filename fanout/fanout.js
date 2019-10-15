73eb4dbff8add4842d34e62fc3efda8860475b92
sd1998 <shashvat51@gmail.com>
2019-10-15 18:21:48 +0530

    Fanout service integrated with zookeeper for configuration

--- fanout/fanout.js
+++ fanout/fanout.js
@@ -1,65 +1,127 @@
-const express = require('express');
 const amqp = require('amqplib');
 const zookeeper = require('node-zookeeper-client');
 const zookeeperClient = zookeeperClient.createClient(config['ZOOKEEPER_URL']);
-const config = require('../config.js');
-const constants = require('../constants.js');
-const app = express();
 
 var publisherChannel = null;
+var consumerChannel = null;
 var isConnectedToZookeeper = false;
 var servicesRunning = false;
+var nodePaths = [];
 
 const PORT = 6001 || process.env.PORT
 
-function publisher(amqpConnection) {
+function publisher(amqpConnection, nodePaths) {
   amqpConnection.createChannel(onPublisherStart);
   function onPublisherStart(err, channel) {
     if (err) {
       console.error(err)
       process.exit(1)
     }
-    channel.assertQueue(config.FAN_OUT_QUEUE)
+    createQueueForNodes(nodePaths)
     publisherChannel = channel
   }
 }
 
-zookeeperClient.on('connected', function() {
-  zookeeperClient.exists(config['ZOOKEEPER_NODES_PATH'], function(err, stat) {
+function consumer(amqpConnection) {
+  amqpConnection.createChannel(onComsumerStart);
+  function onConsumerStart(err, channel) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    channel.assertQueue(config['FAN_OUT_QUEUE'])
+    channel.consume()
+    consumerChannel = channel
+  }
+}
+
+function createQueueForNodes(nodePaths) {
+  if (publisherChannel != null) {
+    for (var node in nodePaths) {
+      publisherChannel.assertQueue(node)
+    }
+  }
+}
+
+function getData(client, path, done) {
+  client.getData(path, function(event) {
+    getData(client, path, done)
+  }, function(err, data, stat) {
     if (err) {
       console.error(err)
       return
     }
     if (stat) {
-      isConnectedToZookeeper = true
-      servicesRunning = true
-      console.log('Connected to zookeeper');
+      done(data.toString('utf8'))
     }
     else {
-      servicesRunning = false
+      done(null)
     }
   })
-})
+}
 
-app.post('/send', function(req, res) {
-  if (isConnectedToZookeeper && servicesRunning) {
-    //zookeeperClient.get
-  }
-  else {
-    res.status(200).json({
-      'status': 'Notification service not running'
-    })
-  }
-})
-
-app.listen(PORT, function() {
-  zookeeperClient.connect()
-  amqp.connect(config.RMQ_URL, function(err, amqpConnection) {
+function getChildren(client, path, done) {
+  client.getChildren(path, function(event) {
+    getChilden(client, path, done)
+  }, function(err, children) {
     if (err) {
       console.error(err)
       return
     }
-    consumer(amqpConnection)
+    done(children)
   })
-  console.log('Fan Out service listening at: ' + PORT)
+}
+
+zookeeperClient.on('connected', function() {
+  if (config == null) {
+    zookeeperClient.exists('/config', function(err, stat) {
+      if (err) {
+        console.error(err)
+        return
+      }
+      if (stat) {
+        isConnectedToZookeeper = true
+        servicesRunning = true
+        console.log('Connected to zookeeper');
+        getData(zookeeperClient, './config', function(data) {
+          config = JSON.parse(data)
+          getChildren(zookeeperClient, config['ZOOKEEPER_NODES_PATH'], function(updatedNodePaths) {
+            newNodePaths = []
+            for (var node in updatedNodePaths) {
+              if (!nodePaths.includes(node)) {
+                newNodePaths.push(node)
+              }
+            }
+            nodePaths = []
+            Array.prototype.push(nodePaths, data.split(','))
+            createQueueForNodes(newNodePaths)
+          })
+        })
+      }
+      else {
+        servicesRunning = false
+      }
+    })
+  }
+})
+
+function consume(message) {
+  if (isConnectedToZookeeper && servicesRunning) {
+    for (var node in nodesPath) {
+      publisherChannel.sendToQueue(node, Buffer.from(JSON.stringify(message.content.toString('utf8'))))
+    }
+    consumerChannel.ack(message)
+  }
+  else {
+    consumerChannel.nack(message)
+  }
+}
+
+zookeeperClient.connect()
+amqp.connect(config.RMQ_URL, function(err, amqpConnection) {
+  if (err) {
+    console.error(err)
+    return
+  }
+  consumer(amqpConnection)
 })
