ed3f7a5da2e62806a6659e4de52a81dab3e030da
sd1998 <shashvat51@gmail.com>
2019-11-06 10:36:21 +0530

    zuul gateway integrated for load balancing

--- external/learning/src/main/java/com/sk/learning/LearningController.java
+++ external/learning/src/main/java/com/sk/learning/LearningController.java
@@ -4,7 +4,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
